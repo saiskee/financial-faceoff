7cc16fdc9d03206b3a64f4295516a3e413904f08
Keerthan Ekbote <saiskee@gmail.com>
2019-08-25 12:36:18 -0400

    Updated Home GUI

--- imports/ui/containers/Home.js
+++ imports/ui/containers/Home.js
@@ -2,11 +2,12 @@ import React from 'react';
 import {withTracker} from "meteor/react-meteor-data";
 import {Games} from '../../api/links';
 import Grid from "@material-ui/core/Grid/index";
-import Button from "@material-ui/core/Button/index";
+import {isMobile} from "react-device-detect";
 import {withRouter} from "react-router";
 import withAudio from "../hoc/withAudio";
 import Paper from "@material-ui/core/Paper";
 import "./Home.css";
+
 
 const styles = {
   home: {
@@ -47,27 +48,21 @@ class Home extends React.Component {
 
               </style>
               <div className="panel-back">
-              <span className={"panel-answer"}
-              onClick={() => {
-                localStorage.removeItem(game._id);
-                this.props.history.push(`/games/${game._id}/regular/0`)}}>Play {game.title}</span>
+                {!isMobile &&
+                <span className={"panel-answer"}
+                      onClick={() => {
+                        localStorage.removeItem(game._id);
+                        this.props.history.push(`/games/${game._id}/regular/0`)}}>Play {game.title}
+              </span>
+                }
                 <span className={"points"}
                 onClick={()=> {this.props.history.push(
                   `/games/${game._id}/controller`)}
-                }>Controller</span>
+                }>Controller
+                  {isMobile && " for " + game.title}</span>
               </div>
               </div>
           </div>
-          {/* <Paper style={styles.paper}>
-            {!isMobile && 
-            <Button color="primary" onClick={() => {
-              localStorage.removeItem(game._id);
-              this.props.history.push(`/games/${game._id}/regular/0`)}}>Play {game.title}
-            </Button>}
-            <Button variant="contained" style={{float:'left'}} onClick={()=> {this.props.history.push(
-          `/games/${game._id}/controller`)}
-        }>Controller</Button>
-          </Paper> */}
         </Grid>
       )
     })
@@ -83,7 +78,7 @@ class Home extends React.Component {
               <h1 style={styles.h1}>Welcome to <span style={{color:'#013220'}}>Financial Faceoff!</span></h1>
               <h2>My Games</h2>
             </Grid>
-            {games.length > 0 ? games: <h3 style={{color:'darkred', paddingLeft:'10px'}}>You havent created any games. Create one now!</h3>}
+            {games.length > 0 ? games: <h3 style={{color:'darkred', paddingLeft:'10px'}}>You haven't created any games. Create one now!</h3>}
           </Grid>
         </Paper>
       </div>
